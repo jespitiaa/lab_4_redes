927a704cb28ed286ed8b8c2194f197a6365e0d7c
SANTIAGO RODRIGUEZ MATIZ <s.rodriguezm@sis.virtual.uniandes.edu.co>
2019-03-21 18:51:16 -0500

    initial commit

---
+++ socketConnect.py
@@ -0,0 +1,51 @@
+import socket, select, sys
+import hashlib, binascii, os
+import json
+
+def hash_password(password):
+    """Hash a password for storing."""
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
+
+
+
+def autenticarme(user, password):
+	message = {
+		"user" : user,
+		"pw" : hash_password(password)
+	}
+	try:
+		s.sendall(json.dumps(message).encode())
+	except:
+		print("Error al enviar las credenciales")
+	reply = s.recv(4096).decode()
+	return reply
+
+
+#Crear conexion
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+	print("No se pudo establecer conexión")
+	sys.exit()
+	
+print("Se creó el socket")
+
+host = "localhost"
+port = 8082
+
+try: 
+	remote_ip = socket.gethostbyname(host)
+except socket.gaierror:
+	print("Hostname no resuelto")
+	sys.exit()
+	
+print("Direccion ipv4: "+ str(remote_ip))
+
+s.connect((remote_ip, port))
+
+print("Se establecio una conexion con " + host)
+
---
+++ socketServer.py
@@ -0,0 +1,35 @@
+import socket, select, sys
+import hashlib, binascii, os
+import json
+
+host = ''
+port = 8082
+
+def verify_password(stored_password, provided_password):
+    """Verify a stored password against one provided by user"""
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512', 
+                                  provided_password.encode('utf-8'), 
+                                  salt.encode('ascii'), 
+                                  100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
+
+	
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host,port))
+except socket.error:
+	print("No se pudo desplegar el servidor")
+	sys.exit()
+
+print("Despliegue correcto del servidor")
+s.listen(1000)
+print("Servidor listo")
+
+conn, addr = s.accept()
+
+print("Conectado con " + addr[0] + ":" + str(addr[1]))
+
+data = conn.recv(1024)
