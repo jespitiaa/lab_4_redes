4f836ece661e854bbf4778b14f99955cd28e2426
jespitiaa <js.espitia@uniandes.edu.co>
2019-03-24 11:31:38 -0500

    gcorregí un error del protocolo de inicio de sesion.voy a probar en unix

--- mainClient.py
+++ mainClient.py
@@ -2,11 +2,12 @@
 import socket, select, string, sys
 import hashlib, binascii, os
 
-
+#Este metodo va a permitir una interaccion dinamica con el usuario cada vez que deba ingresar informacion
 def prompt():
 	sys.stdout.write('<you>')
 	sys.stdout.flush()
 
+#Este metodo va a encriptar la contraseña que el usuario ingrese, de tal forma que al enviarse por el socket, si es interceptada, que no sea legible
 def hash_password(password):
     #Hash a password for storing.
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
@@ -14,59 +15,77 @@ def hash_password(password):
                                 salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
     return (salt + pwdhash).decode('ascii')
+
 if __name__ == "__main__":
+	#Se definen las caracteristicas basicas del socket donde se quiere conectar: Puerto, host, y protocolo (TCP=SOCK_STREAM)
 	host = "localhost"
 	port = 5000
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.settimeout(3)
 
+	#Se intenta establecer una conexion con el socket servidor
 	try:
 		s.connect((host, port))
 	except:
 		print("No se ha podido conectar al servidor")
 		sys.exit()
 
+	#La primera parte de la interaccion consiste en elegir un registro o inicio de sesion
 	print("Para iniciar sesion escribe 1, para registrarte escribe 2")
 	prompt()
 	
+	#La variable status indica si el cliente debe ingresar usuario(1), contrasenia(2), o no ha elegido (0)
 	status = 0
 	
 	while True:
+		#Se definen los dos sockets, el de la conexion y la entrada por consola para que el usuario envie informacion
 		socket_list = [sys.stdin, s]
-
-		read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+		read_sockets, write_sockets = select.select(socket_list, [] )
 		for sock in read_sockets:
-
+			#Este primer socket es el que establece la conexion efectiva con el servidor y recibe la informacion
 			if sock == s:
+				#Siempre esta a la espera de informacion
 				data = sock.recv(4096)
 				if not data:
 					print ("Desconexion")
 					sys.exit()
 				else:
+					print("llego respuesta")
 					sys.stdout.write(data.decode())
+					
+					#Caso en que el usuario ya este autorizado
+					if(resp.decode()=="Ya iniciaste sesion"):
+						sys.exit()
+					
+					elif(resp.decode()== "Ingresa tu Username: "):
+						status = 1
+					elif(resp.decode()== "Password: "):
+						status = 2		
+					elif(resp.decode()== "Usuario no existe"):
+						print("Para iniciar sesion escribe 1, para registrarte escribe 2")
+						status = 0
+					elif(resp.decode()== "Credenciales incorrectas"):
+						print("Password: ")
+						status = 2
+					elif(resp.decode()== "Bienvenido"):
+						sys.exit()
+					elif(resp.decode()== "Usuario creado"):
+						sys.exit()
+					elif(resp.decode()== "Usuario ya existe"):
+						print("Para iniciar sesion escribe 1, para registrarte escribe 2")
+						status = 0
 					prompt()
+			#Este es el caso para el socket que envia informacion desde el terminal
 			else:
-				
 				msg = input()
-				s.send(msg.encode())
-				resp = s.recv(4096)
-				print("llego respuesta")
-				print(resp)
-				if(resp.decode()=="Ya iniciaste sesion"):
-					print("Bienvenido")
-					break
-				if(resp.decode()== "Password: "):
-					pw = input()
-					print(hash_password(pw))
-					s.send(hash_password(pw).encode())
-					auth = s.recv(4096)
-					if(auth.decode() == "No existe"):
-						print("Fallo en la autenticacion")
-					elif(auth.decode() == "No es la contrasenia"):
-						print("Contrasenia incorrecta")
-					else:
-						print("Bienvenido!")
-						break
-				else:
-					continue
+				res = msg
+				
+				#Se formatea el envio de los datos de sesion
+				if(status == 1):
+					res = "user:" + msg
+				if(status == 2):
+					ress = "password:" + msg 
+
+				s.send(res.encode())
+				
 				prompt()
--- mainServer.py
+++ mainServer.py
@@ -1,22 +1,26 @@
-import socket, select
+import socket, select, numpy
 import hashlib, binascii, os
 import persistence as db
 
 if __name__ == "__main__":
-	print("main")
+	#Se tendra un conjunto de sockets conectados
 	CONEXIONES = []
+
+	#Se define un tamanio para el buffer de lectura de 4096b, y el puerto de escucha
 	BUFFER = 4096
 	PORT = 5000
 	
+	#Se definen caracteristicas basicas del servidor y se despliega en un puerto definido
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	server_socket.bind(("localhost", PORT))
-	server_socket.listen(10)
+	server_socket.listen(200)
 	
 	CONEXIONES.append(server_socket)
 	
 	print("Servidor iniciado en el puerto " + str(PORT))
 	
+	#Este metodo se va a encargar de verificar un par de contrasenias encriptadas 
 	def verify_password(stored_password, provided_password):
 		#Verify a stored password against one provided by user
 		salt = stored_password[:64]
@@ -31,75 +35,105 @@ if __name__ == "__main__":
 			if(s==sock):
 				continue
 			sock.send(msg)
+
+	#El arreglo va a indicar cuales sockets ya se autenticaron		
 	allowed_origins = []
 	while True:
+		#Se guardan variables en arreglos para los sockets y el estado de cada uno (Empieza en 0 para todos)
 		read_sockets, write_sockets, error_sockets = select.select(CONEXIONES, [], [])
-		
-		for sock in read_sockets:
+		socket_status = numpy.zeros([len(read_sockets)], dtype=int)
 
+		for sock in read_sockets:
+			username = ""
+			pw = ""
+			index = read_sockets.index(sock)
+			#Acepta constantemente nuevas conexiones al servidor
 			if sock == server_socket:
 				sockfd, addr = server_socket.accept()
 				CONEXIONES.append(sockfd)
 				print("El cliente (%s, %s) está conectado" %addr)
 				
+			#Lee constantemente informacion enviada desde los clientes en la terminal	
 			else:
-				status = 0
-				print(str(status)+"estado")
+				status = socket_status[index]
+				print(str(status)+" estado")
+				#Define el flujo de informacion
 				try:
-					#sock.send(b"Para iniciar sesion escribe 1, para registrarte escribe 2")
-					#if(status==0):
-					#	sock.send(b"Para iniciar sesion escribe 1, para registrarte escribe 2")
+					#Empieza recibiendo informacion del cliente
 					data = sock.recv(BUFFER)
 
-					#recibe
 					#1 autentica 2 crea
 					#llamo mongo
 					#si coincide con el metodo verify, emito un mensaje para llamar lo de rafael
+					#Revisa si existe un cliente que envie informacion efectivamente 
 					if data:
 						#sock.send(data)
-						print(data.decode())
-						if data.decode()=="1" or data.decode() == "2":
-							status = int(data.decode())
-							print(str(status)+"estado")
+						print(data.decode(), type(data.decode()))
 
-							print(data.decode())
+						#Se pregunta por la primera entrada posible, elegir entre login y registro
+						if status == 0 and (data.decode().strip()=="1" or data.decode().strip() == "2") :
+							#se cambia el estado 
+							status = int(data.decode())
+							print(str(status)+"-estado")
+							socket_status[index]= status
+							sock.send(b"Ingresa tu Username: ")
+						#Se recibio el nombre del usuario
+						elif data.decode().startswith('user:'):
+							username = data.decode().split(":")[1]
+							#Se consulta si el usuario ya esta en la base de datos
+							usuario = db.getUser(username)
+							#Caso de inicio de sesion
 							if status == 1:
-								sock.send("Inicio de sesion")
-							else:
-								sock.send("Registro")
-							print("loeoe")
+								#El usuario ya debe existir para pedir la contrasenia. Si no existe, se indica al usuario
+								if usuario == None:
+									status = 0
+									print(str(status)+"-estado")
+									socket_status[index]= status
+									sock.send(b"Usuario no existe")
+								else:
+									sock.send(b"Password: ")
+							#Caso de registro
+							elif status == 2:
+								#El usuario no debe existir para crear uno con la contrasenia que indique el usuario. Si ya existe se avisa
+								if usuario == None:
+									sock.send(b"Password: ")
+								else:
+									status = 0
+									print(str(status)+"-estado")
+									socket_status[index]= status
+									sock.send(b"Usuario ya existe")
+						
+						#Se recibio la contrasenia
+						elif data.decode().startswith('password:'):
+							pw = data.decode().split(":")[1]
+							#Si esta iniciando sesion, se debe validar que las credenciales coincidan
+							if(status==1):
+								usuario = db.getUser(username)
+								if verify_password(usuario.password, pw):
+									sock.send(b"Bienvenido")
+									allowed_origins.append(sock)
+								else:
+									sock.send(b"Credenciales incorrectas")
+							#Si esta registrandose, se crea un usuario con las credenciales indicadas y se inicia la sesion de forma automatica
+							elif(status==2):
+								#Crea el nuevo usuario
+								cred = {
+									"username": username,
+									"password": pw
+								}
+								db.createUser(cred, username)
+								sock.send(b"Usuario creado")
 						else:
 							if status == 0:
-								sock.send("Opcion invalida")
-							else:
-								if(sock not in allowed_origins):
-									sock.send(b"Ingresa tu Username: ")
-									username = sock.recv(4096)
-									print(username)
-									sock.send(b"Password: ")
-									pw = sock.recv(4096)
-									print(pw)
-									if(status == 1):
-										usuario = db.getUser(username)
-										if(usuario == None):
-											sock.send(b"No existe")
-										elif (verify_password(usuario.password, pw)):
-											sock.send(b"Exito")
-											allowed_origins.append(sock)
-										else:
-											sock.send(b"No es la contrasenia")
-									elif status == 2:
-										created = db.createUser(username, pw)
-										if not created:
-											sock.send(b"El usuario ya existe")
-											status == 0
-										else:
-											sock.send(b"Usuario creado")
-											status ==0
-						
+								sock.send(b"Opcion invalida")
+							
+								
+
+				#Se tiene en cuenta el caso en que el cliente se desconecte		
 				except:
 					print("Client (%s,%s) is offline"%addr)
 					sock.close()
 					CONEXIONES.remove(sock)
 					continue
+	#Al final del ciclo infinito (por interrupcion del teclado o falla de hardware) se cierra el socket
 	server_socket.close()
