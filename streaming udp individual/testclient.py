baeadf77a5f2bfef478addea22fcde374a120289
santiago rodriguez matiz <s.rodriguezm@uniandes.edu.co>
2019-03-24 17:16:29 -0500

    a ver si ahora si

---
+++ streaming udp individual/testclient.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import socket
+import threading
+import datetime
+
+msgFromClient = "Hello UDP Server"
+bytesToSend = str.encode(msgFromClient)
+serverAddressPort = ("127.0.0.1", 20001)
+bufferSize = 46080
+
+class MyThread2(threading.Thread):
+    num=0
+    def darNombre(self, pnum):
+            num=pnum
+    def run(self):
+        recibidos=0
+        inicio=0
+        fin=0
+        delta=0
+        # Create a UDP socket at client side
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        # Send to server using created UDP socket
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        isRecording=True
+        s="".encode('ascii')
+        while(True):
+            msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+            inicio=datetime.datetime.now().time()
+            print("recibi paquete")
+            recibidos=recibidos+1
+            print(str(recibidos))
+            if("{}".format(msgFromServer[0])=="fin"):
+                break
+                f.write("fin")
+                f.write("Se recibieron: "+str(recibidos)+ " paquetes en el cliente"+num)
+                fin=datetime.datetime.now().time()
+                delta=fin-inicio
+                f.write("Tiempo de atencion cliente: "+str(delta))
+        print('cliente '+str(x+1)+' termino')
+f=open("UDPClientResults.txt","a+")
+for x in range(0,5):
+    mythread2= MyThread2()
+    numero=str(x+1)
+    mythread2.darNombre(numero)
+    mythread2.start()
+    print('cliente '+str(x+1)+' comenzo')
+    #f.write("Cliente %d\r\n" % (x+1))
---
+++ streaming udp individual/testserver.py
@@ -0,0 +1,58 @@
+import socket
+import cv2
+import threading
+
+localIP     = "127.0.0.1"
+localPort   = 20001
+bufferSize  = 46080
+
+msgFromServer = "Hello UDP Client"
+bytesToSend = str.encode(msgFromServer)
+addresses=[]
+
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+
+cap = cv2.VideoCapture('bh.mp4')
+f=open("UDPServerResults.txt","a+")
+class MyThread2(threading.Thread):
+    def run(self):
+        enviados=0
+        while(True):
+            rep, frame = cap.read()
+            # Sending a reply to client
+            if rep == True:
+                for i in range(20):
+                    for ad in addresses:
+                        UDPServerSocket.sendto(frame.flatten().tostring()[i*46080:(i+1)*46080], ad)
+                        print("envie un paquete")
+                        enviados=enviados+1
+                        print(str(enviados))
+                        #print(str(rep))
+            else:
+                for ad in addresses:
+                    UDPServerSocket.sendto(str.encode("fin"), ad)
+                    print("termino cliente"+ str(ad))
+                    f.write("Se enviaron: "+str(enviados)+" paquetes a direccion: "+str(ad))
+                break
+
+print("UDP server up and listening")
+
+mythread2 = MyThread2()
+mythread2.start()
+
+while True:
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    address = bytesAddressPair[1]
+    mythread2.isAlive()
+    print(address)
+    if address not in addresses:
+        addresses.append(address)
+
+
+
+print('hola')
+# Listen for incoming datagrams
