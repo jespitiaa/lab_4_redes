c8f0ff228e9c9e78bac07d8ae1453f09e4cf6b55
Rafael Tejón <rc.tejon@uniandes.edu.co>
2019-03-24 20:48:22 -0500

    listas de reproduccion

--- streaming udp individual/client.py
+++ streaming udp individual/client.py
@@ -2,38 +2,41 @@ import numpy as np
 import cv2
 import socket
 
-msgFromClient = "Hello UDP Server"
-bytesToSend = str.encode(msgFromClient)
-serverAddressPort = ("127.0.0.1", 20001)
-bufferSize = 46080
+def runClient(vid='1,2,3'):
+    bytesToSend = str.encode(vid)
+    serverAddressPort = ("127.0.0.1", 20001)
+    bufferSize = 46080
 
-# Se crea un socket udp cliente
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # Se crea un socket udp cliente
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# se envia un mensaje al servidor para comenzar la conexion
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    # se envia un mensaje al servidor para comenzar la conexion
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-isRecording=True
-s="".encode('ascii')
-while(True):
-    #se reciven los paquetes transmitidos por el servidor
-    data, addr = UDPClientSocket.recvfrom(bufferSize)
-    #se concatenan los paquetes para formar threads
-    s = s+data
-    if len(s)==(46080*20):
-        #se forma el frame
-        frame = np.fromstring(s, dtype='uint8')
-        frame = frame.reshape(480, 640, 3)
-        s="".encode('ascii')
-        if(isRecording):
-            cv2.imshow('frame', frame)#se muestra el frame generado
-        if cv2.waitKey(1) & 0xFF == ord('q'):#parar transmision
-            break
-        if cv2.waitKey(1) & 0xFF == ord('p'):#Pause
-            print('Pause')
-            isRecording=False
-        if cv2.waitKey(1) & 0xFF == ord('c'):#Continue
-            print('Continue')
-            isRecording=True
-cap.release()
-cv2.destroyAllWindows()
+    isRecording=True
+    s="".encode('ascii')
+    while(True):
+        print('.')
+        #se reciven los paquetes transmitidos por el servidor
+        data, addr = UDPClientSocket.recvfrom(bufferSize)
+        #se concatenan los paquetes para formar threads
+        s = s+data
+        if len(s)==(46080*20):
+            #se forma el frame
+            frame = np.fromstring(s, dtype='uint8')
+            frame = frame.reshape(480, 640, 3)
+            s="".encode('ascii')
+            if(isRecording):
+                cv2.imshow('frame', frame)#se muestra el frame generado
+            if cv2.waitKey(1) & 0xFF == ord('q'):#parar transmision
+                break
+            if cv2.waitKey(1) & 0xFF == ord('p'):#Pause
+                print('Pause')
+                isRecording=False
+            if cv2.waitKey(1) & 0xFF == ord('c'):#Continue
+                print('Continue')
+                isRecording=True
+    cap.release()
+    cv2.destroyAllWindows()    
+
+runClient()
--- streaming udp individual/server.py
+++ streaming udp individual/server.py
@@ -8,7 +8,13 @@ bufferSize  = 46080
 
 msgFromServer = "Hello UDP Client"
 bytesToSend = str.encode(msgFromServer)
+global addresses
 addresses=[]
+global vid
+vid=[]
+global kkk
+kkk=False
+
 
 #Se crea el socket udp del servidor
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -17,20 +23,41 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 
 #se inicia la captura del video a transmitir
-cap = cv2.VideoCapture('bebecita.mp4')
+global cap 
+cap = cv2.VideoCapture('med.mp4')
+
+def nuevoVid(idVid):
+    global cap
+    print(idVid)
+    if idVid=='1':
+        cap = cv2.VideoCapture('bh.mp4')
+    elif idVid=='2':
+        cap = cv2.VideoCapture('peq.mp4')
+    elif idVid=='3':
+        cap = cv2.VideoCapture('med.mp4')    
 
 class MyThread2(threading.Thread):
     def run(self):
         while(True):
-            #se toma un nuevo frame del video
-            rep, frame = cap.read()
-            if rep == True:
-                for i in range(20):
-                    for ad in addresses:
-                        #se dividen los frames en paquetes que se transmiten a todas las direcciones de los clientes conectados
-                        UDPServerSocket.sendto(frame.flatten().tostring()[i*46080:(i+1)*46080], ad)
+            global kkk
+            if kkk:      
+                #se toma un nuevo frame del video
+                rep, frame = cap.read()
+                if rep == True:
+                    for i in range(20):
+                        for ad in addresses:
+                            #se dividen los frames en paquetes que se transmiten a todas las direcciones de los clientes conectados
+                            UDPServerSocket.sendto(frame.flatten().tostring()[i*46080:(i+1)*46080], ad)
+                else:
+                    kkk=False
+                    print('finVid')
             else:
-                break
+                print(kkk)
+                if len(vid)>0:
+                    print('inicioVid')
+                    print(vid)
+                    nuevoVid(vid.pop())
+                    kkk=True
 
 print("UDP server up and listening")   
 
@@ -41,6 +68,7 @@ mythread2.start()
 while True:
     #Se reciben nuevas conexiones con clientes y se añaden a las direcciones a las cual se transmitira el video
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    vid=bytesAddressPair[0].decode().split(',')
     address = bytesAddressPair[1]
     mythread2.isAlive()
     print(address)
